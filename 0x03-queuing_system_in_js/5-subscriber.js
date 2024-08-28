86e8b9970127431aee4f81747891cbdc915b6819
Profxbaba <daddyslove41@gmail.com>
2024-08-28 14:33:30 +0000

    Queuing System in JS

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,26 @@
+import redis from 'redis';
+
+// Create Redis client
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+// Subscribe to the channel
+client.subscribe('holberton school channel');
+
+// Listen for messages on the channel
+client.on('message', (channel, message) => {
+  console.log(message);
+
+  // If the message is 'KILL_SERVER', unsubscribe and quit
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe();
+    client.quit();
+  }
+});
